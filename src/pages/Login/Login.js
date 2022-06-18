02c4a64027eea9aee3014e94996f87d57c2b5c6f
Rahim2725 <info.abdurrahim12@gmail.com>
2022-06-19 02:57:42 +0600

    react from done

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom"
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import Loading from '../../components/Loading';
+import { useForm } from 'react-hook-form';
 
 
 const Login = () => {
+
     let errorMessage;
 
     const [
@@ -14,18 +16,24 @@ const Login = () => {
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
-    errorMessage= <p className='text-red-500'>{error?.message}</p>
+    errorMessage = <p className='text-red-500'>{error?.message}</p>
 
     console.log(user);
 
-    const handleLogin = event => {
-        event.preventDefault();
-        console.log('btn click')
-        const email = event.target.email.value;
-        const password = event.target.password.value;
-        signInWithEmailAndPassword(email, password);
+    // use normal login from 
+    // const handleLogin = event => {
+    //     event.preventDefault();
+    //     console.log('btn click')
+    //     const email = event.target.email.value;
+    //     const password = event.target.password.value;
+    //     signInWithEmailAndPassword(email, password);
 
-    }
+    // }
+    
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
 
     if (loading) {
         <Loading></Loading>
@@ -36,19 +44,25 @@ const Login = () => {
         <div class="hero min-h-screen bg-base-200">
             <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                 <div class="card-body">
-                    <form onSubmit={handleLogin}>
+
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <div class="form-control">
                             <label class="label">
                                 <span class="label-text">Email</span>
                             </label>
-                            <input type="text" name='email' placeholder="email" class="input input-bordered" />
+                            <input {...register("firstName", { required: true })} />
+                            {errors.firstName?.type === 'required' && "First name is required"}
                         </div>
 
                         <div class="form-control">
                             <label class="label">
                                 <span class="label-text">Password</span>
                             </label>
-                            <input type="text" name='password' placeholder="password" class="input input-bordered" />
+
+                            <input {...register("lastName", { required: true })} />
+                            {errors.lastName && "Last name is required"}
+
                             <label class="label">
                                 <Link to="/forgotPassword" class="label-text-alt link link-hover">Forgot password?</Link>
                             </label>
